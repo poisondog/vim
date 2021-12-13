a132e91c6671b2b6584655129fa03ec7e351ee58
Adam Huang <poisondog@gmail.com>
2021-12-13 17:32:55 +0800

    create a find java function script by python

---
+++ script/findJavaFunction.py
@@ -0,0 +1,9 @@
+import re
+import sys
+
+regex = r"^\s*(private|public|protected).*\s(\S+)\s*\(.*\)\s*{\s*$"
+
+for line in sys.argv:
+	matches = re.finditer(regex, line, re.MULTILINE)
+	for matchNum, match in enumerate(matches, start=1):
+		print(match.group(2))
